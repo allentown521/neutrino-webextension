3fb5a6ed5f15746466f458dc3f1bd2206e19cdb0
allentown <tangxiangpi@gmail.com>
2024-11-24 16:08:45 +0800

    refactor: 重构 WebExtension 开发环境配置
    - 移除了对 Manifest V2 的支持，仅保留 Manifest V3
    - 更新了 WexExtManifestPlugin 以直接处理不同的 manifest 版本
    - 删除了单独的 manifestV2 和 manifestV3 模块
    - 调整了 package.json 中的依赖和脚本
    - 简化了 webpack 配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,1 @@
-const neutrino = require('neutrino');
-
-const confManifestV2 = require('./.neutrinorc-v2.js');
-
-const configV3 = neutrino().webpack(); // by default neutrino load .neutrinorc.js
-const configV2 = neutrino(confManifestV2).webpack();
-
-module.exports = [
-  configV2,
-  configV3
-];
+module.exports = require('neutrino')().webpack()
